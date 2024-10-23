62fb000ff6ded664dfeee0e5fda2e9bb4221adad
FranGoncal <gfrancisco@ipcbcampus.pt>
2024-10-23 18:31:34 +0530

    Aula 6/7

--- app.py
+++ app.py
@@ -2,13 +2,18 @@ from counterfit_connection import CounterFitConnection
 CounterFitConnection.init('127.0.0.1', 5000)
 import time
 from counterfit_shims_grove.adc import ADC
+from counterfit_shims_grove.grove_relay import GroveRelay
 
 adc = ADC()
+relay = GroveRelay(7)
 
 while True:
-    soil_moisture = adc.read(0)
-    print("Soil moisture:", soil_moisture)
+	relay.on()
+	time.sleep(.5)
+	relay.off()
+	soil_moisture = adc.read(0)
+	print("Soil moisture:", soil_moisture)
 
-    time.sleep(10)
+	time.sleep(10)
 
 
