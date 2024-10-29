383d5992cacba00e2345afe9a022a17ac9817f09
FranGoncal <gfrancisco@ipcbcampus.pt>
2024-10-30 00:26:03 +0530

    Resolução Bug no Automated plant watering

--- app.py
+++ app.py
@@ -1,48 +1,27 @@
 from counterfit_connection import CounterFitConnection
 CounterFitConnection.init('127.0.0.1', 5000)
+
 import time
 from counterfit_shims_grove.adc import ADC
 from counterfit_shims_grove.grove_relay import GroveRelay
-import paho.mqtt.client as mqtt
 import json
-import threading
+import paho.mqtt.client as mqtt
 
+id = 'asdffdas'
 
-id = '52313255'
 client_name = id + 'soilmoisturesensor_client'
-
-mqtt_client = mqtt.Client(client_name)
-mqtt_client.connect('test.mosquitto.org')
-mqtt_client.loop_start()
-print("MQTT connected!")
-
-water_time = 5
-wait_time = 20
-
-client_telemetry_topic = id + '/telemetry'
 server_command_topic = id + '/commands'
-
+client_telemetry_topic = id + '/telemetry'
 
 adc = ADC()
 relay = GroveRelay(7)
 
-def send_relay_command(client, state):
-    command = { 'relay_on' : state }
-    print("Sending message:", command)
-    client.publish(server_command_topic, json.dumps(command))
+mqtt_client = mqtt.Client(client_name)
+mqtt_client.connect('test.mosquitto.org')
 
-def control_relay(client):
-    print("Unsubscribing from telemetry")
-    mqtt_client.unsubscribe(client_telemetry_topic)
+mqtt_client.loop_start()
 
-    send_relay_command(client, True)
-    time.sleep(water_time)
-    send_relay_command(client, False)
-
-    time.sleep(wait_time)
-
-    print("Subscribing to telemetry")
-    mqtt_client.subscribe(client_telemetry_topic)
+print("MQTT connected!")
 
 def handle_command(client, userdata, message):
 	payload = json.loads(message.payload.decode())
@@ -52,21 +31,23 @@ def handle_command(client, userdata, message):
 		relay.on()
 	else:
 		relay.off()
-
-def handle_telemetry(client, userdata, message):
-    payload = json.loads(message.payload.decode())
-    print("Message received:", payload)
-
-    if payload['soil_moisture'] > 450:
-        threading.Thread(target=control_relay, args=(client,)).start()
-
+        
 mqtt_client.subscribe(server_command_topic)
 mqtt_client.on_message = handle_command
 
 while True:
 	soil_moisture = adc.read(0)
 	print("Soil moisture:", soil_moisture)
-
-	mqtt_client.publish(client_telemetry_topic, json.dumps({'soil_moisture' : soil_moisture}))
+	telemetry = json.dumps({'soil_moisture' : soil_moisture})
+	print("Sending telemetry ", telemetry)
+	#cliente manda um telemetry
+	mqtt_client.publish(client_telemetry_topic, telemetry)
 
 	time.sleep(10)
+
+
+
+
+
+
+
--- server2.py
+++ server2.py
@@ -2,8 +2,9 @@ import json
 import time
 
 import paho.mqtt.client as mqtt
+import threading
 
-id = '52313255'
+id = 'asdffdas'
 
 client_telemetry_topic = id + '/telemetry'
 server_command_topic = id + '/commands'
@@ -14,18 +15,48 @@ mqtt_client.connect('test.mosquitto.org')
 
 mqtt_client.loop_start()
 
+water_time = 5
+wait_time = 20
+
+def send_relay_command(client, state):
+	#state = true | false
+    command = { 'relay_on' : state }
+    print("Sending message:", command)
+    client.publish(server_command_topic, json.dumps(command))
+
+def control_relay(client):
+	#5 nao le mensagnes
+    print("Unsubscribing from telemetry")
+    mqtt_client.unsubscribe(client_telemetry_topic)
+
+	#6manda true para o relay (ligar)
+    send_relay_command(client, True)
+    	#7 espera 5 segundos
+    time.sleep(water_time)
+	#8 desliga o relay (false)
+    send_relay_command(client, False)
+	#9 espera 20 segundos
+    time.sleep(wait_time)
+
+	#10 volta a ler as mensagens
+    print("Subscribing to telemetry")
+    mqtt_client.subscribe(client_telemetry_topic)
+
 def handle_telemetry(client, userdata, message):
-	payload = json.loads(message.payload.decode())
-	print("Message received:", payload)
-	command = { 'relay_on' : payload['soil_moisture'] > 450 }
-	print("Sending message:", command)
-
-	client.publish(server_command_topic, json.dumps(command))
-
-
+	#1 le o nivel
+    payload = json.loads(message.payload.decode())
+    print("Message received:", payload)
+	#2 verifica o nivel
+    if payload['soil_moisture'] > 450:
+    	#3 chama o control_relay(client)
+        threading.Thread(target=control_relay, args=(client,)).start()
 
 mqtt_client.subscribe(client_telemetry_topic)
 mqtt_client.on_message = handle_telemetry
 
 while True:
     time.sleep(2)
+    
+    
+    
+    
